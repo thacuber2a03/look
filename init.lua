cc50b2761cdb72e3793815def9ba03e8d93701c2
thacuber2a03 <antonidiaz200731@gmail.com>
2024-03-31 17:28:23 -0400

    many changes, check full commit message
    - `disp_attrib` is callable now, same behavior as `.new`
    - adding a value into an integer index in `disp_attrib` puts it in the attributes property, other keys are an error
    - `disp_attrib` can now take a table with nothing in it or just nothing
    - removed `or "4"` (`config.replace_italic` can't be set to nil, so this
      operation was useless)
    - added `disp_attrib:nocolor()`, removes all color information from this
      attribute
    - fixed bug where if color was disabled, it would not apply any
      of the attributes

--- init.lua
+++ init.lua
@@ -1,24 +1,24 @@
---- MIT License
----
---- Copyright (c) 2024 @thacuber2a03
----
---- Permission is hereby granted, free of charge, to any person obtaining a copy
---- of this software and associated documentation files (the "Software"), to deal
---- in the Software without restriction, including without limitation the rights
---- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
---- copies of the Software, and to permit persons to whom the Software is
---- furnished to do so, subject to the following conditions:
----
---- The above copyright notice and this permission notice shall be included in all
---- copies or substantial portions of the Software.
----
---- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
---- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
---- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
---- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
---- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
---- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
---- SOFTWARE.
+-- MIT License
+--
+-- Copyright (c) 2024 @thacuber2a03
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be included in all
+-- copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+-- SOFTWARE.
 
 local look = {}
 look.version = "0.1"
@@ -50,7 +50,7 @@ look.colors = {}
 
 setmetatable(look, { __index = function(_, k) return look.attributes[k] or look.colors[k] end })
 
-local disp_attrib = {}
+local disp_attrib = setmetatable({}, { __call = function(cls, ...) return cls.new(...) end })
 look.disp_attrib = disp_attrib
 
 function disp_attrib:__index(k)
@@ -58,36 +58,36 @@ function disp_attrib:__index(k)
 	return rawget(disp_attrib, k)
 end
 
-local function in_range(v, a, b) return v >= a and v <= b end
+function disp_attrib:__newindex(k, v)
+	local t = type(k)
+	if t == "number" then self.attributes[k] = v end
+	error("attempt to index a display attribute with a " .. t)
+end
 
-function disp_attrib.new(a)
+local function is_color(v) return v >= 30 and v <= 50 or v >= 80 and v <= 100 end
+
+function disp_attrib(a)
 	do
 		local t = type(a)
-		assert(t == "table" or t == "number", "expected a list of attributes or a single attribute")
-		if t == "table" then
-			assert(#a ~= 0, "no attributes for display attribute initialization")
-		else
-			a = {a}
-		end
+		assert(not a or t == "number" or t == "table", "expected a list of attributes, a single attribute, or nothing")
+		if t ~= "table" then a = {a} end
 	end
 
 	local self = setmetatable({}, disp_attrib)
 
 	self.attributes = a
 	self.has_color = false
-	self.color_mode = nil
 
 	local attrib_str = ""
+
 	for i,v in ipairs(self.attributes) do
 		if v == 3 and config.replace_italic then
-			attrib_str = attrib_str .. (look[config.replace_italic] or "4") -- underline
+			attrib_str = attrib_str .. look[config.replace_italic]
 		elseif v ~= 0 then
 			attrib_str = attrib_str .. v
 		end
 
-		if in_range(v, 30, 50) or in_range(80, 100) then
-			self.has_color = true
-		end
+		if is_color(v) then self.has_color = true end
 
 		if i ~= #self.attributes then attrib_str = attrib_str .. ';' end
 	end
@@ -97,32 +97,60 @@ function disp_attrib.new(a)
 	return self
 end
 
-function disp_attrib.instance(v)
-	return type(v) == "table" and getmetatable(v) == disp_attrib
+function disp_attrib.instance(v) return type(v) == "table" and getmetatable(v) == disp_attrib end
+
+function disp_attrib:escaped() return (self.code:gsub("\x1b", "<esc>")) end
+
+function disp_attrib:nocolor()
+	local res = disp_attrib()
+	local skip = 0
+
+	for i, a in ipairs(self.attributes) do
+		if skip > 0 then
+			skip = skip - 1
+		else
+			if is_color(a) then
+				if a == 38 or a == 48 then
+					local id = self.attributes[i+1]
+					if id == 2 then skip = 5
+					elseif id == 5 then skip = 3
+					end
+				end
+			else
+				res[i] = a
+			end
+		end
+	end
+
+	return res
 end
 
 function disp_attrib:__call(other) return other + self end
 
 function disp_attrib:__add(other)
 	if not disp_attrib.instance(self) then
-		if not config.color and other.has_color then return self end
+		if not config.color and other.has_color then
+			return self .. other:nocolor().code
+		end
 		return self .. other.code
 
 	elseif type(other) == "string" then
-		if not config.color and self.has_color then return other end
+		if not config.color and self.has_color then
+			return self:nocolor().code + other
+		end
 		return self.code .. other
 
 	elseif type(other) == "number" then
 		local res = {}
 		for i=1, #self.attributes do res[#res+1] = self.attributes[i] end
 		res[#res+1] = other
-		return disp_attrib.new(res)
+		return disp_attrib(res)
 
 	elseif disp_attrib.instance(other) then
 		local res = {}
 		for i=1, #self.attributes do res[#res+1] = self.attributes[i] end
 		for i=1, #other.attributes do res[#res+1] = other.attributes[i] end
-		return disp_attrib.new(res)
+		return disp_attrib(res)
 
 	else
 		error("attempt to concatenate display attribute with "..type(other))
@@ -131,57 +159,55 @@ end
 
 function disp_attrib:__tostring() return self.code end
 
-function disp_attrib:escaped() return (self.code:gsub("\x1b", "<esc>")) end
-
 do
 	local attr = look.attributes
-	attr.normal           = disp_attrib.new(0)
+	attr.normal           = disp_attrib(0)
 	attr.reset            = attr.normal
-	attr.bold             = disp_attrib.new(1)
-	attr.dim              = disp_attrib.new(2)
-	attr.italic           = disp_attrib.new(3)
-	attr.underline        = disp_attrib.new(4)
-	attr.blink            = disp_attrib.new(5)
-	attr.fastblink        = disp_attrib.new(6)
-	attr.invert           = disp_attrib.new(7)
-	attr.conceal          = disp_attrib.new(8)
-	attr.strike           = disp_attrib.new(9)
+	attr.bold             = disp_attrib(1)
+	attr.dim              = disp_attrib(2)
+	attr.italic           = disp_attrib(3)
+	attr.underline        = disp_attrib(4)
+	attr.blink            = disp_attrib(5)
+	attr.fastblink        = disp_attrib(6)
+	attr.invert           = disp_attrib(7)
+	attr.conceal          = disp_attrib(8)
+	attr.strike           = disp_attrib(9)
 
-	attr.double_underline = disp_attrib.new(21)
+	attr.double_underline = disp_attrib(21)
 	attr.nobold           = attr.doubleul
-	attr.noblink          = disp_attrib.new(25)
-	attr.noinvert         = disp_attrib.new(27)
-	attr.reveal           = disp_attrib.new(28)
+	attr.noblink          = disp_attrib(25)
+	attr.noinvert         = disp_attrib(27)
+	attr.reveal           = disp_attrib(28)
 	attr.noconceal        = attr.reveal
-	attr.nostrike         = disp_attrib.new(29)
+	attr.nostrike         = disp_attrib(29)
 
-	attr.default_font = disp_attrib.new(10)
+	attr.default_font = disp_attrib(10)
 	for i=1, 9 do
-		attr["font"..i] = disp_attrib.new(10+i)
+		attr["font"..i] = disp_attrib(10+i)
 	end
-	attr.gothic       = disp_attrib.new(20)
+	attr.gothic       = disp_attrib(20)
 
 	local col = look.colors
-	col.black   = disp_attrib.new{30}
-	col.red     = disp_attrib.new{31}
-	col.green   = disp_attrib.new{32}
-	col.yellow  = disp_attrib.new{33}
-	col.blue    = disp_attrib.new{34}
-	col.magenta = disp_attrib.new{35}
-	col.cyan    = disp_attrib.new{36}
-	col.white   = disp_attrib.new{37}
-	col.default = disp_attrib.new{39}
+	col.black   = disp_attrib(30)
+	col.red     = disp_attrib(31)
+	col.green   = disp_attrib(32)
+	col.yellow  = disp_attrib(33)
+	col.blue    = disp_attrib(34)
+	col.magenta = disp_attrib(35)
+	col.cyan    = disp_attrib(36)
+	col.white   = disp_attrib(37)
+	col.default = disp_attrib(39)
 
-	for k,v in pairs(col) do col["bright_"..k] = disp_attrib.new{v[1] + 60} end
-	for k,v in pairs(col) do col["bg_"..k]     = disp_attrib.new{v[1] + 10} end
+	for k,v in pairs(col) do col["bright_"..k] = disp_attrib(v[1] + 60) end
+	for k,v in pairs(col) do col["bg_"..k]     = disp_attrib(v[1] + 10) end
 
 	-- these don't exist
 	col.bright_default = nil
 	col.bg_bright_default = nil
 
-	col.from_rgb = function(r, g, b) return disp_attrib.new{38, 2, r, g, b} end
-	col.bg_from_rgb = function(r, g, b) return disp_attrib.new{48, 2, r, g, b} end
-	col.from_index = function(i) return disp_attrib.new{38, 2, i} end
+	col.from_rgb = function(r, g, b) return disp_attrib{38, 2, r, g, b} end
+	col.bg_from_rgb = function(r, g, b) return disp_attrib{48, 2, r, g, b} end
+	col.from_index = function(i) return disp_attrib{38, 2, i} end
 end
 
 function look.format(str)
